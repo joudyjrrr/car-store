3a25f0e0835a79674a0466d4e0ce8b931608fec4
joudyjrrr <joudyjrrr@gmail.com>
2024-05-19 23:09:29 +0300

    first commit

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -4,11 +4,13 @@ import Axios, {
   InternalAxiosRequestConfig,
 } from 'axios'
 import { toast } from 'react-hot-toast'
-export const BASE_URL_IMG = `https://carstore.htc-company.com/getImage`
+// export const BASE_URL_IMG = `https://carstore.htc-company.com/getImage`
+export const BASE_URL_IMG = `http://127.0.0.1:8000/getImage`
+
 
 const axios: AxiosInstance = Axios.create({
-  baseURL: "https://carstore.htc-company.com/api/dashboard",
-  // baseURL: "http://127.0.0.1:8000/api/dashboard",
+  // baseURL: "https://carstore.htc-company.com/api/dashboard",
+  baseURL: "http://127.0.0.1:8000/api/dashboard",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
